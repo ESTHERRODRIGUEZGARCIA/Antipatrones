62b306e1c4d9a36a98a3698c21edb9224e04f45e
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2023-11-24 18:08:05 +0100

    django hecho

--- micalculadora/calculadora/views.py
+++ micalculadora/calculadora/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+# calculadora/views.py
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import JsonResponse
+
+def calcular(num1, num2, operacion):
+    try:
+        num1 = float(num1)
+        num2 = float(num2)
+
+        if operacion == 'suma':
+            resultado = num1 + num2
+        elif operacion == 'resta':
+            resultado = num1 - num2
+        elif operacion == 'multiplicacion':
+            resultado = num1 * num2
+        elif operacion == 'division':
+            if num2 != 0:
+                resultado = num1 / num2
+            else:
+                return JsonResponse({'error': 'No se puede dividir entre cero.'})
+        else:
+            return JsonResponse({'error': 'Operación no soportada.'})
+
+        # Puedes guardar el historial de operaciones si es necesario
+        # HistorialOperaciones.objects.create(operacion=operacion, resultado=resultado)
+
+        return JsonResponse({'resultado': resultado})
+
+    except ValueError:
+        return JsonResponse({'error': 'Los números ingresados no son válidos.'})
+
+def calculadora(request):
+    return render(request, 'calculadora/index.html')
